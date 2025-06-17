78829546618b8366b4de2fc7194f8f99ae56ba3e
Stephen Naylor <engsnayl@gmail.com>
2025-06-17 07:49:39 +0000

    Updarte config.py with the final Secret Manager code so no more hard coded creds

--- config.py
+++ config.py
@@ -1,6 +1,34 @@
-# config.py - TEMPORARY for demonstration only
-# WARNING: This is NOT safe for production! We'll fix it with Secrets Manager.
+import boto3
+from botocore.exceptions import ClientError
+import json
 
-AWS_ACCESS_KEY_ID = "AKIAIOSFODNN7EXAMPLE"
-AWS_SECRET_ACCESS_KEY = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
-AWS_REGION = "us-east-2"
+
+def get_secret():
+
+    secret_name = "AWS-Credentials-for-NextWork-Dummy-IAM-Account"
+    region_name = "eu-west-1"
+
+    # Create a Secrets Manager client
+    session = boto3.session.Session()
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=region_name
+    )
+
+    try:
+        get_secret_value_response = client.get_secret_value(
+            SecretId=secret_name
+        )
+    except ClientError as e:
+        # For a list of exceptions thrown, see
+        # https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+        raise e
+
+    secret = get_secret_value_response['SecretString']
+    return json.loads(secret)
+    
+credentials = get_secret()
+
+AWS_ACCESS_KEY_ID = credentials.get("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = credentials.get("AWS_SECRET_ACCESS_KEY")
+AWS_REGION = credentials.get("AWS_REGION", boto3.session.Session().region_name or "eu-west-1")
